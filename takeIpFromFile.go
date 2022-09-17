82dcd17c938cd9a1de0ec11d1f7de73c60a27fce
MajoSupInOffice <majohead@gmail.com>
2022-09-17 06:45:33 +0300

    Я добавил вывод данных в файл. Но надо изменить и сделать, чтобы IP-адреса выводились через пробел.

--- takeIpFromFile.go
+++ takeIpFromFile.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
-	"regexp"
 	"io/ioutil"
 	"log"
+	"os"
+	"regexp"
 )
 
-func main()  {
-content, err := ioutil.ReadFile("/home/ryazanov/Downloads/tdlfFile.txt")
+func main() {
+	content, err := ioutil.ReadFile("/home/ryazanov/Downloads/tdlfFile.txt")
 
 	if err != nil {
 		log.Fatal(err)
@@ -19,10 +20,21 @@ content, err := ioutil.ReadFile("/home/ryazanov/Downloads/tdlfFile.txt")
 	re := regexp.MustCompile(`(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}`)
 
 	fmt.Printf("Pattern: %v\n", re.String()) // print pattern
-	fmt.Println(re.MatchString(str1)) // true
+	fmt.Println(re.MatchString(str1))        // true
 
 	submatchall := re.FindAllString(str1, -1)
 	for _, element := range submatchall {
 		fmt.Println(element)
+	}
+
+	// Теперь необходимо записать полученные данные в новый файл
+	f, err := os.Create("onlyIP.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	for _, element2 := range submatchall {
+		_, err = f.WriteString(element2)
 	}
 }
