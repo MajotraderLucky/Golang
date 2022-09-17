3f5a8f6cc37d383741a6953eff3d1d832c9c6f68
MajoSupInOffice <majohead@gmail.com>
2022-09-17 04:50:58 +0300

    Завершил написание кода.

--- takeIpFromFile.go
+++ takeIpFromFile.go
@@ -8,5 +8,21 @@ import (
 )
 
 func main()  {
-content, err := ioutil.ReadFile("")	
+content, err := ioutil.ReadFile("/home/ryazanov/Downloads/tdlfFile.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	str1 := string(content)
+
+	re := regexp.MustCompile(`(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}`)
+
+	fmt.Printf("Pattern: %v\n", re.String()) // print pattern
+	fmt.Println(re.MatchString(str1)) // true
+
+	submatchall := re.FindAllString(str1, -1)
+	for _, element := range submatchall {
+		fmt.Println(element)
+	}
 }
