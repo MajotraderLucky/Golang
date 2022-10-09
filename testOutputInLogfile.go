deaa794550d0e1d75945014fe8c107d7defde22f
MajoTraderLucky <majohead@gmail.com>
2022-10-09 14:57:10 +0300

    Были внесены изменения в файл testOutputInLogfile.go

--- testOutputInLogfile.go
+++ testOutputInLogfile.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"regexp"
 	"io/ioutil"
+	"log"
 	"os"
+	"reflect"
+	"regexp"
+	"strings"
 )
 
-func main(){
-	takeContent, err := ioutil.ReadFile("/home/ryazanov/Downloads/tdlfFile.txt")
+func main() {
+	takeContent, err := ioutil.ReadFile("/home/sergey/Downloads/tdlfFile.txt")
 
 	if err != nil {
 		log.Fatal(err)
@@ -39,10 +41,10 @@ func main(){
 	// Мы фильтруем данные, которые получаем из файла tdlfFile.txt,
 	// и помещаем отфильтрованные IP-адреса в созданный файл testLog.log
 	// Всё протестировано и всё работает нормально.
-	// Теперь надо создать переменную и присвоить ей строку IP-адресов из 
+	// Теперь надо создать переменную и присвоить ей строку IP-адресов из
 	// файла testLog.log
 
-	ipList, err := ioutil.ReadFile("/home/ryazanov/MyCode/GoCode/MajorGo/Monitoring/testLog.log")
+	ipList, err := ioutil.ReadFile("/home/sergey/MyCode/GoCode/Golang/testLog.log")
 
 	if err != nil {
 		log.Fatal(err)
@@ -51,5 +53,45 @@ func main(){
 	fmt.Println(pRe.MatchString(listIpInString)) // true
 
 	takeFirstIp := pRe.FindString(listIpInString)
-	
+	fmt.Println(takeFirstIp, " - Выводим переменную takeFirstIp")
+
+	// Сначала необходимо создать массив данных и присвоить ему первый IP
+	// Далее нам необходимо удалить это IP-адрес из файла testLog.log
+
+	// Проверим тип переменной takeFirstIp
+	fmt.Println("Тип переменной takeFirstIp -", reflect.TypeOf(takeFirstIp))
+	// Удалим пробелы, если есть, в строчной переменной takeFirstIp
+	takeFirstIp = strings.ReplaceAll(takeFirstIp, " ", "")
+
+	// Создаём массив, где будут храниться Ip-адреса, которые мы будем извлекать по
+	// одному из файла testLog.log
+
+	var ipInStringArr [500]string
+	var indexForArr int = 0 // Индекс массива, который будет увеличиваться инкрементом
+	// после того, как строка с ip-адресом будет положена в массив.
+	ipInStringArr[indexForArr] = takeFirstIp
+	fmt.Print("Массиву с индексом [", indexForArr, "] было присвоено значение ", ipInStringArr[0], "\n")
+	indexForArr++
+	fmt.Println("Индекс массива indexForArr увеличился и принял значение -", indexForArr)
+
+	// Дальше посчитаем, количество символов в строчной переменной
+	// takeFirstIp
+
+	howManyLetters := (len(takeFirstIp)) + 2
+	fmt.Println("В строке ", takeFirstIp, "-", howManyLetters, "символов.")
+
+	// Теперь удалим количество знаков переменной howManyLetters
+	// из строчной переменной listIpInString
+	subListIpInString := listIpInString[howManyLetters:]
+	//fmt.Print(subListIpInString)
+
+	// Теперь создадим новый файл и запишем в него строчную переменную subListIpInString для проверки
+	createNewLogFile, err := os.Create("newTestLog.log")
+	if err != nil {
+		panic(err)
+	}
+	defer createNewLogFile.Close()
+
+	createNewLogFile.Write([]byte(subListIpInString))
+
 }
